33baacc92fc88edf6926879e791a0daef5ac011f
MrD0NTPLAY <jrconley93@gmail.com>
2025-02-20 21:28:25 -0500

    Add files via upload

---
+++ Documents/m-game-solo.sh
@@ -0,0 +1,5 @@
+G
+pactl load-module module-remap-sink sink_name=Game
+pactl load-module module-remap-sink sink_name=Chat
+pactl load-module module-remap-sink sink_name=Music
+pactl load-module module-remap-sink sink_name=System
--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
+fish_config theme choose "Dracula Official"
 if status is-interactive
     # Commands to run in interactive sessions can go here
-fastfetch
+fastfetch --color '4;92' -l /home/jconz/Pictures/vampteeth.ini
 end
+
+fish_add_path /home/jconz/.spicetify
