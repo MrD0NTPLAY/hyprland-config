596e33e7170ad3c6e9d8923c2b13eac948e33786
MrD0NTPLAY <jrconley93@gmail.com>
2025-03-08 01:47:24 -0500

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 fish_config theme choose "Dracula Official"
 if status is-interactive
     # Commands to run in interactive sessions can go here
-fastfetch --color '4;92' -l /home/jconz/Pictures/vampteeth.ini
+fastfetch
 end
 
 fish_add_path /home/jconz/.spicetify
